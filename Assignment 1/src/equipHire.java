fe08a06cc99481caf729833412523c17c46a7248
Daniel Wheeler <wheedan@gmail.com>
2017-06-08 16:30:43 +0000

    Fixed input issue Added output for part 1 Unable to test

--- Assignment 1/src/equipHire.java
+++ Assignment 1/src/equipHire.java
@@ -15,8 +15,8 @@ class equipHire {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String cust_Name;
-		String cust_address;
-		String cust_phone;
+		String cust_Address;
+		String cust_Phone;
 		String cust_Dl;
 		String cust_CCnum;
 		String cust_CCexp;
@@ -24,12 +24,13 @@ class equipHire {
 		String equip_Type;
 		double day_Rate;
 		
+		// User input
 		System.out.print("Enter Customer Name: ");
 		cust_Name = sc.nextLine();
 		System.out.print("Enter Customer Address: ");
-		cust_address = sc.nextLine();
+		cust_Address = sc.nextLine();
 		System.out.print("Enter Customer Phone Number: ");
-		cust_phone = sc.nextLine();
+		cust_Phone = sc.nextLine();
 		System.out.print("Enter Customer Drivers Licence: ");
 		cust_Dl = sc.nextLine();
 		System.out.print("Enter Customer Credit Card Number: ");
@@ -38,11 +39,27 @@ class equipHire {
 		cust_CCexp = sc.nextLine();
 		System.out.print("Enter Booking Length in Days: ");
 		book_Length = sc.nextInt();
+		sc.nextLine();
 		System.out.print("Enter Equipment Type to be hired: ");
 		equip_Type = sc.nextLine();
 		System.out.print("Enter Daily Hire Rate: ");
 		day_Rate = sc.nextDouble();
 		double basic_Charge=(book_Length*day_Rate);
+		
+		// invoice output
+		
+		System.out.print("Customer Details");
+		System.out.print("----------------");
+		System.out.printf("Name: %30s",cust_Name);
+		System.out.printf("Address: %27s",cust_Address);
+		System.out.printf("Phone Number: %22s",cust_Phone);
+		System.out.printf("DL No: %29s",cust_Dl);
+		System.out.printf("CC Number: %25s",cust_CCnum);
+		System.out.printf("CC expiry: %25s",cust_CCexp);
+		System.out.printf("");
+		System.out.printf("");
+		System.out.printf("");
+		System.out.printf("");
 		
 
 
