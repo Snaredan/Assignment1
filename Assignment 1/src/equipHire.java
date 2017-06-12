b7f888fc6551ca7f2f919ec8fc0142d40bacdb3b
Snaredan <wheedan@gmail.com>
2017-06-12 22:33:24 +1000

    Minor changes, comments indicating next steps

--- Assignment 1/src/equipHire.java
+++ Assignment 1/src/equipHire.java
@@ -1,17 +1,15 @@
 /**
  * 
 /**
- * @author Snarebear
- *
+ * @author s3655764
+ *Branch of Assignment 1 for implementation of stage 2 requirements
  */
 
 import java.util.*;
 
 class equipHire {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String cust_Name;
@@ -21,7 +19,7 @@ class equipHire {
 		String cust_CCnum;
 		String cust_CCexp;
 		int book_Length;
-		String equip_Type;
+		String equip_Code;
 		double day_Rate;
 		
 		// User input
@@ -40,11 +38,13 @@ class equipHire {
 		System.out.print("Enter Booking Length in Days: ");
 		book_Length = sc.nextInt();
 		sc.nextLine();
-		System.out.print("Enter Equipment Type to be hired: ");
-		equip_Type = sc.nextLine();
-		System.out.print("Enter Daily Hire Rate: ");
-		day_Rate = sc.nextDouble();
+		System.out.print("Enter Equipment Code: ");
+		equip_Code = sc.nextLine();
+		// todo: determine equipment type and day hire rate from equip code
+		
 		double basic_Charge=(book_Length*day_Rate);
+		
+		// todo: determine discount (if any) from Booking length
 		
 		// invoice output
 		
@@ -58,12 +58,12 @@ class equipHire {
 		System.out.printf("CC expiry: %25s\r\n",cust_CCexp);
 		System.out.print("Equipment Hire Details\r\n");
 		System.out.print("----------------------\r\n");
-		System.out.printf("Equipment Description: %9s\r\n",equip_Type);
+		System.out.printf("Equipment Description: %9s\r\n",equip_Code);
 		System.out.printf("Booking Length (Days): %9d\r\n",book_Length);
 		System.out.printf("Daily Hire Rate: %15f\r\n",day_Rate);
 		System.out.printf("Base Hire Charge: %14f\r\n",basic_Charge);
 		
-
+		
 
 	}
 
